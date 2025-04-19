748e1388478b544b60959d4b6d26a4ba2efab3bf
Chris Kamper <navigator4223@gmail.com>
2025-04-19 17:25:53 +0200

    better ~/update.sh

--- executable_update.sh
+++ executable_update.sh
@@ -1,10 +1,17 @@
 #! /bin/bash
 
+set -e
+
+echo "--- chezmoi ---"
 cd ~/.local/share/chezmoi
 git pull
 chezmoi apply
 
+echo "--- updating system via script ---"
 cd ~/repos/kde-plasma
 git pull
 ./install.sh
 
+echo "--- updating visor ---"
+cd kwin-scripts/visor
+./update.sh
