a61678fd68c9124fa63104385e83966fa5ca3707
Chris Kamper <navigator4223@gmail.com>
2025-07-09 04:22:00 +0200

    added more hyprland improvements

---
+++ scripts/executable_hypredit.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+file="$1"
+abs_path="$(realpath "$file")"
+is_managed=$(chezmoi managed "$abs_path")
+
+if [[ -n "$is_managed" ]]; then
+  echo "🧠 Chezmoi edit: $abs_path"
+  chezmoi edit "$abs_path"
+else
+  echo "✍️  Nvim edit (not managed): $abs_path"
+  nvim "$abs_path"
+fi
