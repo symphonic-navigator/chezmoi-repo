103472709f01441930fb71ce7545750fc6a2dbf5
Chris Kamper <navigator4223@gmail.com>
2025-07-09 04:44:45 +0200

    added more hyprland improvements

--- private_dot_local/bin/executable_hypredit.sh
+++ private_dot_local/bin/executable_hypredit.sh
@@ -7,6 +7,7 @@ is_managed=$(chezmoi managed "$abs_path")
 if [[ -n "$is_managed" ]]; then
   echo "🧠 Chezmoi edit: $abs_path"
   chezmoi edit "$abs_path"
+  chezmoi apply "$abs_path"
 else
   echo "✍️  Nvim edit (not managed): $abs_path"
   nvim "$abs_path"
