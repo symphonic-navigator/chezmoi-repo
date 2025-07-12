0a52d5da65338c53f338dbadd876ad82146e8c36
symphonic-navigator <navigator4223@gmail.com>
2025-07-12 08:32:39 +0200

    much improved ncspot workspace

--- private_dot_local/bin/executable_hyprland-keep-ncspot-alive.sh
+++ private_dot_local/bin/executable_hyprland-keep-ncspot-alive.sh
@@ -4,13 +4,15 @@ set -e
 set -u
 
 CLASS_NAME="kitty-music"
-SESSION_FILE=~/.config/kitty/sessions/ncspot.conf
+SESSION_FILE=~/.config/kitty/sessions/ncspot.session
+BASE_CONFIG_FILE=~/.config/kitty/kitty.conf
+ADDITIONAL_CONFIG_FILE=~/.config/kitty/sessions/ncspot.conf
 
 QUERY=".[] | select(.class == \"$CLASS_NAME\")"
 
 if ! hyprctl clients -j | jq -e "$QUERY" >/dev/null; then
   echo "[$(date)] $CLASS_NAME not found, launching..."
-  kitty --class "$CLASS_NAME" --session "$SESSION_FILE" &
+  kitty -c "$BASE_CONFIG_FILE" -c "$ADDITIONAL_CONFIG_FILE" --class "$CLASS_NAME" --session "$SESSION_FILE" &
 else
   echo "[$(date)] $CLASS_NAME is already running."
 fi
