997beafbdd865f8171710a48a1eb3288e9dec090
symphonic-navigator <navigator4223@gmail.com>
2025-08-28 07:39:51 +0200

    using official spotify client for now

--- private_dot_local/bin/executable_hyprland-keep-ncspot-alive.sh
+++ private_dot_local/bin/executable_hyprland-keep-ncspot-alive.sh
@@ -3,16 +3,21 @@
 set -e
 set -u
 
-CLASS_NAME="kitty-music"
-SESSION_FILE=~/.config/kitty/sessions/ncspot.session
-BASE_CONFIG_FILE=~/.config/kitty/kitty.conf
-ADDITIONAL_CONFIG_FILE=~/.config/kitty/sessions/ncspot.conf
+#CLASS_NAME="kitty-music"
+#SESSION_FILE=~/.config/kitty/sessions/ncspot.session
+#BASE_CONFIG_FILE=~/.config/kitty/kitty.conf
+#ADDITIONAL_CONFIG_FILE=~/.config/kitty/sessions/ncspot.conf
+
+CLASS_NAME="Spotify"
+#SESSION_FILE=~/.config/kitty/sessions/ncspot.session
+#BASE_CONFIG_FILE=~/.config/kitty/kitty.conf
+#ADDITIONAL_CONFIG_FILE=~/.config/kitty/sessions/ncspot.conf
 
 QUERY=".[] | select(.class == \"$CLASS_NAME\")"
 
 if ! hyprctl clients -j | jq -e "$QUERY" >/dev/null; then
   echo "[$(date)] $CLASS_NAME not found, launching..."
-  kitty -c "$BASE_CONFIG_FILE" -c "$ADDITIONAL_CONFIG_FILE" --class "$CLASS_NAME" --session "$SESSION_FILE" &
+  flatpak run com.spotify.Client &
 else
   echo "[$(date)] $CLASS_NAME is already running."
 fi
