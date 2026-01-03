713501e938c637b23e69b16c93ef79bbb0888083
symphonic-navigator <navigator4223@gmail.com>
2026-01-03 12:44:56 +0100

    added personal and work mode

---
+++ private_dot_local/bin/executable_setup-personal.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+cp ~/.local/share/tk-workspace/tabs.json.personal ~/.local/share/tk-workspace/tabs.json
+
+echo "✅ tk setup for personal use"
---
+++ private_dot_local/bin/executable_setup-work.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+cp ~/.local/share/tk-workspace/tabs.json.work ~/.local/share/tk-workspace/tabs.json
+
+echo "✅ tk setup for work"
