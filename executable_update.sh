da5e3c4daa7c6c150cada2b446b8ce1cb1253d90
symphonic-navigator <navigator4223@gmail.com>
2026-01-05 08:58:20 +0100

    better scratchpads behaviour

--- executable_update.sh
+++ executable_update.sh
@@ -6,12 +6,3 @@ echo "--- chezmoi ---"
 cd ~/.local/share/chezmoi
 git pull
 chezmoi apply
-
-echo "--- updating system via script ---"
-cd ~/repos/kde-plasma
-git pull
-./install.sh
-
-echo "--- updating visor ---"
-cd kwin-scripts/visor
-./update.sh
